709bc71096a03e54d8a96420121a73d52f580cee
zaeminkim <106994679+zaeminkim@users.noreply.github.com>
2026-03-31 23:29:49 +0900

    update: CGP 5주차 과제코드

--- _myApp_/_myApp_.cpp
+++ _myApp_/_myApp_.cpp
@@ -1,97 +1,101 @@
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+
 #include <sb7.h>
-#include <vmath.h>
+#include <shader.h>
 
 class my_application : public sb7::application
 {
 public:
-	// 쉐이더를 컴파일한다
 	GLuint compile_shaders(void)
 	{
 		GLuint vertex_shader;
 		GLuint fragment_shader;
 		GLuint program;
 
-		// 버텍스 쉐이더 소스코드를 쉐이더 객체로 전달한다.
-		static const GLchar* vertex_shader_source[] =
-		{
-			"#version 430 core                  \n"
-			"                                   \n"
-			"void main(void)                    \n"
-			"{                                  \n"
-			"gl_Position=vec4(0.0,0.0,0.5,1.0); \n"
-			"}                                  \n"
-		};
-		// 프래그먼트 쉐이더 소스코드를 쉐이더 객체로 전달한다.
-		static const GLchar* fragment_shader_source[] =
-		{
-			"#version 430 core                  \n"
-			"                                   \n"
-			"out vec4 color;                    \n"
-			"void main(void)                    \n"
-			"{                                  \n"
-			"color=vec4(0.0, 0.8, 1.0, 1.0);    \n"
-			"}                                  \n"
-		};
+		vertex_shader = sb7::shader::load("pinwheel_vs.glsl", GL_VERTEX_SHADER);
+		fragment_shader = sb7::shader::load("pinwheel_fs.glsl", GL_FRAGMENT_SHADER);
 
-		// 버텍스 쉐이더를 생성, 전달, 컴파일 한다.
-		vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
-		glCompileShader(vertex_shader);
 
-		// 프래그먼트 쉐이더를 생성, 전달, 컴파일 한다.
-		fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
-		glCompileShader(fragment_shader);
-
-		// 프로그램을 생성하고 쉐이더를 어태치시키고 링크한다.
 		program = glCreateProgram();
 		glAttachShader(program, vertex_shader);
 		glAttachShader(program, fragment_shader);
 		glLinkProgram(program);
 
-		// 쉐이더 객체를 삭제한다.
 		glDeleteShader(vertex_shader);
 		glDeleteShader(fragment_shader);
 
 		return program;
 	}
 
-	// 애플리케이션 초기화를 수행한다
+	GLuint compile_shaders2(void) {
+		GLuint vertex_shader;
+		GLuint fragment_shader;
+		GLuint program;
+
+		vertex_shader = sb7::shader::load("stick_vs.glsl", GL_VERTEX_SHADER);
+		fragment_shader = sb7::shader::load("stick_fs.glsl", GL_FRAGMENT_SHADER);
+
+		program = glCreateProgram();
+		glAttachShader(program, vertex_shader);
+		glAttachShader(program, fragment_shader);
+		glLinkProgram(program);
+
+		glDeleteShader(vertex_shader);
+		glDeleteShader(fragment_shader);
+
+		return program;
+	}
+
+
 	virtual void startup()
 	{
 		rendering_program = compile_shaders();
+		rendering_program2 = compile_shaders2();
 		glGenVertexArrays(1, &vertex_array_object);
 		glBindVertexArray(vertex_array_object);
 	}
 
-	// 애플리케이션이 종료될 때 호출된다
 	virtual void shutdown()
 	{
 		glDeleteVertexArrays(1, &vertex_array_object);
 		glDeleteProgram(rendering_program);
+		glDeleteProgram(rendering_program2);
 	}
 
-	// 렌더링 virtual 함수를 작성해서 오버라이딩한다
 	virtual void render(double currentTime)
 	{
-		const GLfloat red[] = {
+		const GLfloat background[] = {
+			0.0f,
+			0.0f,
+			0.0f,
+			1.0f
+		};
+		glClearBufferfv(GL_COLOR, 0, background);
+
+		glUseProgram(rendering_program2);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glUseProgram(rendering_program);
+
+		glVertexAttrib1f(3, (float)currentTime);
+
+		GLfloat color_change[] = {
 			(float)sin(currentTime) * 0.5f + 0.5f,
 			(float)cos(currentTime) * 0.5f + 0.5f,
 			0.0f,
 			1.0f
 		};
-		glClearBufferfv(GL_COLOR, 0, red);
 
-		// 렌더링을 위해 만든 프로그램을 사용하도록 한다
-		glUseProgram(rendering_program);
+		glVertexAttrib4fv(0, color_change);
 
-		// 점을 하나 그린다
-		glPointSize(80.0f);
-		glDrawArrays(GL_POINTS, 0, 1);
+		glDrawArrays(GL_TRIANGLES, 0, 12);
+
 	}
 
 private:
 	GLuint rendering_program;
+	GLuint rendering_program2;
 	GLuint vertex_array_object;
 };
 
--- _transform_/_transform_.cpp
+++ _transform_/_transform_.cpp
@@ -1,97 +1,313 @@
 #include <sb7.h>
-#include <vmath.h>
 
 class my_application : public sb7::application
 {
 public:
-	// 쉐이더를 컴파일한다
 	GLuint compile_shaders(void)
 	{
 		GLuint vertex_shader;
 		GLuint fragment_shader;
 		GLuint program;
 
-		// 버텍스 쉐이더 소스코드를 쉐이더 객체로 전달한다.
 		static const GLchar* vertex_shader_source[] =
 		{
 			"#version 430 core                  \n"
 			"                                   \n"
+			"layout (location = 0) in vec4 color;                          \n"
+			"layout (location = 3) in float time;                          \n"
+			"out vec4 vs_color; \n"
 			"void main(void)                    \n"
 			"{                                  \n"
-			"gl_Position=vec4(0.0,0.0,0.5,1.0); \n"
-			"}                                  \n"
+			"const vec4 vertices[12] = vec4[12](vec4(-0.5,0.5,0.5,1.0),    \n"
+			"								    vec4(0.0,0.5,0.5,1.0),     \n"
+			"								    vec4(0.0,0.0,0.5,1.0),     \n"
+			"                                                              \n"
+			"								    vec4(0.5,0.5,0.5,1.0),     \n"
+			"								    vec4(0.5,0.0,0.5,1.0),     \n"
+			"								    vec4(0.0,0.0,0.5,1.0),     \n"
+			"                                                              \n"
+			"								    vec4(0.5,-0.5,0.5,1.0),    \n"
+			"								    vec4(0.0,-0.5,0.5,1.0),    \n"
+			"								    vec4(0.0,0.0,0.5,1.0),     \n"
+			"                                                              \n"
+			"								    vec4(-0.5,-0.5,0.5,1.0),   \n"
+			"								    vec4(-0.5,0.0,0.5,1.0),    \n"
+			"								    vec4(0.0,0.0,0.5,1.0));    \n"
+
+			// 쉐이더에서 행렬을 정의할 때 열단위로 한 줄씩 정의한다
+			// 바람개비 회전행렬
+			"mat4 rot_mat;\n"
+			"rot_mat[0]=vec4(cos(time), sin(time), 0, 0);\n"
+			"rot_mat[1]=vec4(-sin(time), cos(time), 0, 0);\n"
+			"rot_mat[2]=vec4(0, 0, 1, 0);\n"
+			"rot_mat[3]=vec4(0, 0, 0, 1);\n"
+
+			// 바람개비 이동행렬
+			"mat4 trans_mat;\n"
+			"trans_mat[0]=vec4(1,0,0,0);\n"
+			"trans_mat[1]=vec4(0,1,0,0);\n"
+			"trans_mat[2]=vec4(0,0,1,0);\n"
+			"trans_mat[3]=vec4(sin(time),0,0,1);\n"
+
+			// 모델 메트릭스 선언
+			"mat4 model_mat; \n"
+			"model_mat = trans_mat * rot_mat; \n"
+
+
+			// 뷰 메트릭스 선언
+			"vec3 eyePos = vec3(0.2,0.3,0.8);\n"
+			"vec3 target = vec3(0.0,0.0,0.5);\n"
+			"vec3 upVec = vec3(0.0,1.0,0.0);\n"
+
+			"vec3 D = normalize(eyePos-target);\n"
+			"vec3 R = normalize(cross(upVec, D));\n"
+			"vec3 U = cross(D, R);\n"
+
+			"mat4 view_mat1; \n"
+			"view_mat1[0]=vec4(R.x, U.x, D.x, 0);\n"
+			"view_mat1[1]=vec4(R.y, U.y, D.y, 0);\n"
+			"view_mat1[2]=vec4(R.z, U.z, D.z, 0);\n"
+			"view_mat1[3]=vec4(0, 0, 0, 1);\n"
+
+			"mat4 view_mat2; \n"
+			"view_mat2[0]=vec4(1,0,0,0);\n"
+			"view_mat2[1]=vec4(0,1,0,0);\n"
+			"view_mat2[2]=vec4(0,0,1,0);\n"
+			"view_mat2[3]=vec4(-eyePos.x, -eyePos.y, -eyePos.z, 1);\n"
+
+			"mat4 view_mat;\n"
+			"view_mat = view_mat1 * view_mat2;\n"
+
+
+			// 프로젝션 메트릭스 선언
+			"float left=-0.1;\n"
+			"float right=0.1;\n"
+			"float top=0.07;\n"
+			"float bottom=-0.07;\n"
+			"float near=0.1;\n"
+			"float far=10;\n"
+
+			"mat4 proj_mat; \n"
+			"proj_mat[0]=vec4(2*near/(right-left),0,0,0);\n"
+			"proj_mat[1]=vec4(0,2*near/(top-bottom),0,0);\n"
+			"proj_mat[2]=vec4((right+left)/(right-left),(top+bottom)/(top-bottom),(near+far)/(near-far),-1.0);\n"
+			"proj_mat[3]=vec4(0,0,2*near*far/(near-far),0);\n"
+
+			"gl_Position = proj_mat * view_mat * model_mat * vertices[gl_VertexID];                             \n"
+			// 행렬은 오른쪽에서 왼쪽 방향으로 계산됨.
+			// Scaling -> Rotation -> Translation 순서로 계산해야 함.
+
+			"	const vec4 colors[12] = vec4[12](                          \n"
+			"                           vec4(1.0,0.0,0.0,1.0),            \n"
+			"							vec4(0.0,1.0,0.0,1.0),            \n"
+			"							vec4(0.0,0.0,1.0,1.0),            \n"
+			"                           vec4(1.0,0.0,0.0,1.0),            \n"
+			"							vec4(0.0,1.0,0.0,1.0),            \n"
+			"							vec4(0.0,0.0,1.0,1.0),            \n"
+			"                           vec4(1.0,0.0,0.0,1.0),            \n"
+			"							vec4(0.0,1.0,0.0,1.0),            \n"
+			"							vec4(0.0,0.0,1.0,1.0),            \n"
+			"                           vec4(1.0,0.0,0.0,1.0),            \n"
+			"							vec4(0.0,1.0,0.0,1.0),            \n"
+			"							vec4(0.0,0.0,1.0,1.0));            \n"
+
+			"	vs_color=colors[gl_VertexID] + color;                      \n"
+			"}															   \n"
 		};
-		// 프래그먼트 쉐이더 소스코드를 쉐이더 객체로 전달한다.
+
 		static const GLchar* fragment_shader_source[] =
 		{
 			"#version 430 core                  \n"
-			"                                   \n"
+			"in vec4 vs_color;                  \n"
 			"out vec4 color;                    \n"
 			"void main(void)                    \n"
 			"{                                  \n"
-			"color=vec4(0.0, 0.8, 1.0, 1.0);    \n"
+			"	color=vs_color;                 \n"
 			"}                                  \n"
 		};
 
-		// 버텍스 쉐이더를 생성, 전달, 컴파일 한다.
+
 		vertex_shader = glCreateShader(GL_VERTEX_SHADER);
 		glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
 		glCompileShader(vertex_shader);
 
-		// 프래그먼트 쉐이더를 생성, 전달, 컴파일 한다.
 		fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 		glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
 		glCompileShader(fragment_shader);
 
-		// 프로그램을 생성하고 쉐이더를 어태치시키고 링크한다.
 		program = glCreateProgram();
 		glAttachShader(program, vertex_shader);
 		glAttachShader(program, fragment_shader);
 		glLinkProgram(program);
 
-		// 쉐이더 객체를 삭제한다.
 		glDeleteShader(vertex_shader);
 		glDeleteShader(fragment_shader);
 
 		return program;
 	}
 
-	// 애플리케이션 초기화를 수행한다
+	GLuint compile_shaders2(void) {
+		GLuint vertex_shader;
+		GLuint fragment_shader;
+		GLuint program;
+
+		static const GLchar* vertex_shader_source[] =
+		{
+			"#version 430 core                  \n"
+			"                                   \n"
+			"layout (location = 0) in vec4 color;                          \n"
+			"layout (location = 3) in float time; \n"
+			"out vec4 vs_color; \n"
+			"void main(void)                    \n"
+			"{                                  \n"
+			"const vec4 vertices[6] = vec4[6](    \n"
+			"        vec4(-0.03, -0.8, 0.5, 1.0),         \n"
+			"        vec4( 0.03, -0.8, 0.5, 1.0),         \n"
+			"        vec4(-0.03,  0.0, 0.5, 1.0),         \n"
+
+			"        vec4( 0.03, -0.8, 0.5, 1.0),         \n"
+			"        vec4( 0.03,  0.0, 0.5, 1.0),         \n"
+			"        vec4(-0.03,  0.0, 0.5, 1.0));        \n"
+
+			// 쉐이더에서 행렬을 정의할 때 열단위로 한 줄씩 정의한다
+
+			// 막대기 이동행렬
+			"mat4 trans_mat;\n"
+			"trans_mat[0]=vec4(1,0,0,0);\n"
+			"trans_mat[1]=vec4(0,1,0,0);\n"
+			"trans_mat[2]=vec4(0,0,1,0);\n"
+			"trans_mat[3]=vec4(sin(time),0,0,1);\n"
+
+			// 모델 메트릭스 선언
+			"mat4 model_mat; \n"
+			"model_mat = trans_mat; \n"
+
+
+			// 뷰 메트릭스 선언
+			"vec3 eyePos = vec3(0.2,0.3,0.8);\n"
+			"vec3 target = vec3(0.0,0.0,0.5);\n"
+			"vec3 upVec = vec3(0.0,1.0,0.0);\n"
+
+			"vec3 D = normalize(eyePos-target);\n"
+			"vec3 R = normalize(cross(upVec, D));\n"
+			"vec3 U = cross(D, R);\n"
+
+			"mat4 view_mat1; \n"
+			"view_mat1[0]=vec4(R.x, U.x, D.x, 0);\n"
+			"view_mat1[1]=vec4(R.y, U.y, D.y, 0);\n"
+			"view_mat1[2]=vec4(R.z, U.z, D.z, 0);\n"
+			"view_mat1[3]=vec4(0, 0, 0, 1);\n"
+
+			"mat4 view_mat2; \n"
+			"view_mat2[0]=vec4(1,0,0,0);\n"
+			"view_mat2[1]=vec4(0,1,0,0);\n"
+			"view_mat2[2]=vec4(0,0,1,0);\n"
+			"view_mat2[3]=vec4(-eyePos.x, -eyePos.y, -eyePos.z, 1);\n"
+
+			"mat4 view_mat;\n"
+			"view_mat = view_mat1 * view_mat2;\n"
+
+
+			// 프로젝션 메트릭스 선언
+			"float left=-0.1;\n"
+			"float right=0.1;\n"
+			"float top=0.07;\n"
+			"float bottom=-0.07;\n"
+			"float near=0.1;\n"
+			"float far=10;\n"
+
+			"mat4 proj_mat; \n"
+			"proj_mat[0]=vec4(2*near/(right-left),0,0,0);\n"
+			"proj_mat[1]=vec4(0,2*near/(top-bottom),0,0);\n"
+			"proj_mat[2]=vec4((right+left)/(right-left),(top+bottom)/(top-bottom),(near+far)/(near-far),-1.0);\n"
+			"proj_mat[3]=vec4(0,0,2*near*far/(near-far),0);\n"
+
+			"gl_Position = proj_mat * view_mat * model_mat * vertices[gl_VertexID];                             \n"
+			// Scaling -> Rotation -> Translation 순서로 계산해야 함.
+			"}															   \n"
+		};
+
+		static const GLchar* fragment_shader_source[] =
+		{
+			"#version 430 core                  \n"
+			"in vec4 vs_color;                                   \n"
+			"out vec4 color;                    \n"
+			"void main(void)                    \n"
+			"{                                  \n"
+			"	color=vec4(0.0, 0.8, 1.0, 1.0);              \n"
+			"}                                  \n"
+		};
+
+
+		vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
+		glCompileShader(vertex_shader);
+
+		fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
+		glCompileShader(fragment_shader);
+
+		program = glCreateProgram();
+		glAttachShader(program, vertex_shader);
+		glAttachShader(program, fragment_shader);
+		glLinkProgram(program);
+
+		glDeleteShader(vertex_shader);
+		glDeleteShader(fragment_shader);
+
+		return program;
+	}
+
+
 	virtual void startup()
 	{
 		rendering_program = compile_shaders();
+		rendering_program2 = compile_shaders2();
 		glGenVertexArrays(1, &vertex_array_object);
 		glBindVertexArray(vertex_array_object);
 	}
 
-	// 애플리케이션이 종료될 때 호출된다
 	virtual void shutdown()
 	{
 		glDeleteVertexArrays(1, &vertex_array_object);
 		glDeleteProgram(rendering_program);
+		glDeleteProgram(rendering_program2);
 	}
 
-	// 렌더링 virtual 함수를 작성해서 오버라이딩한다
 	virtual void render(double currentTime)
 	{
-		const GLfloat red[] = {
+		const GLfloat background[] = {
+			0.0f,
+			0.0f,
+			0.0f,
+			1.0f
+		};
+		glClearBufferfv(GL_COLOR, 0, background);
+
+		glUseProgram(rendering_program2);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glUseProgram(rendering_program);
+
+		glVertexAttrib1f(3, (float)currentTime);
+
+		GLfloat color_change[] = {
 			(float)sin(currentTime) * 0.5f + 0.5f,
 			(float)cos(currentTime) * 0.5f + 0.5f,
 			0.0f,
 			1.0f
 		};
-		glClearBufferfv(GL_COLOR, 0, red);
 
-		// 렌더링을 위해 만든 프로그램을 사용하도록 한다
-		glUseProgram(rendering_program);
+		glVertexAttrib4fv(0, color_change);
 
-		// 점을 하나 그린다
-		glPointSize(80.0f);
-		glDrawArrays(GL_POINTS, 0, 1);
+		glDrawArrays(GL_TRIANGLES, 0, 12);
+
 	}
 
 private:
 	GLuint rendering_program;
+	GLuint rendering_program2;
 	GLuint vertex_array_object;
 };
 
