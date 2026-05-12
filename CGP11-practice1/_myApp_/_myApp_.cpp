6cdf1d975da36368021b34620fcd5d7e16464a90
zaeminkim <106994679+zaeminkim@users.noreply.github.com>
2026-05-12 16:57:39 +0900

    CGP11주차 실습코드 주석 추가

--- CGP11-practice1/_myApp_/_myApp_.cpp
+++ CGP11-practice1/_myApp_/_myApp_.cpp
@@ -308,6 +308,11 @@ public:
 		//*/
 
 		// 라이팅 설정 ---------------------------------------
+		//vmath::vec3 lightAmbient(0.2f, 0.2f, 0.2f);
+		//vmath::vec3 lightDiffuse(1.0f, 0.5f, 0.5f);
+		//vmath::vec3 lightSpecular(1.0f, 1.0f, 1.0f);
+		//vmath::vec3 lightPosition = vmath::vec3((float)sin(currentTime * 0.5f), 0.25f, (float)cos(currentTime * 0.5f) * 0.7f);
+
 		vmath::vec3 viewPos = eye;
 		float shininess = 64.f;
 
@@ -334,12 +339,24 @@ public:
 		glUniform3fv(glGetUniformLocation(shader_programs[1], "viewPos"), 1, viewPos);
 
 		// 1. dirLight
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "dirLight.ambient"), 1, lightAmbient);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "dirLight.diffuse"), 1, lightDiffuse);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "dirLight.specular"), 1, lightSpecular);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "viewPos"), 1, viewPos);
+		//glUniform3f(glGetUniformLocation(shader_programs[1], "dirLight.direction"), -0.0f, -1.0f, -0.0f);
 		glUniform3f(glGetUniformLocation(shader_programs[1], "dirLight.direction"), -0.2f, -1.0f, -0.3f);
 		glUniform3f(glGetUniformLocation(shader_programs[1], "dirLight.ambient"), 0.05f, 0.05f, 0.05f);
 		glUniform3f(glGetUniformLocation(shader_programs[1], "dirLight.diffuse"), 0.4f, 0.4f, 0.4f);
 		glUniform3f(glGetUniformLocation(shader_programs[1], "dirLight.specular"), 0.5f, 0.5f, 0.5f);
 
 		// 2. pointLights
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "pointLights[0].ambient"), 1, lightAmbient);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "pointLights[0].diffuse"), 1, lightDiffuse);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "pointLights[0].specular"), 1, lightSpecular);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "viewPos"), 1, viewPos);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "pointLights[0].position"), 1, lightPosition);
+		//glUniform1f(glGetUniformLocation(shader_programs[1], "pointLights[0].c1"), 0.7f);
+		//glUniform1f(glGetUniformLocation(shader_programs[1], "pointLights[0].c2"), 1.8f);
 		glUniform3fv(glGetUniformLocation(shader_programs[1], "pointLights[0].position"), 1, pointLightPositions[0]);
 		glUniform3f(glGetUniformLocation(shader_programs[1], "pointLights[0].ambient"), 0.05f, 0.05f, 0.05f);
 		glUniform3f(glGetUniformLocation(shader_programs[1], "pointLights[0].diffuse"), 0.8f, 0.8f, 0.8f);
@@ -355,6 +372,15 @@ public:
 		glUniform1f(glGetUniformLocation(shader_programs[1], "pointLights[1].c2"), 0.032f);
 
 		// 3. spotLight
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "spotLight.ambient"), 1, lightAmbient);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "spotLight.diffuse"), 1, lightDiffuse);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "spotLight.specular"), 1, lightSpecular);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "viewPos"), 1, viewPos);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "spotLight.position"), 1, eye);
+		//glUniform3fv(glGetUniformLocation(shader_programs[1], "spotLight.direction"), 1, center - eye);
+		//glUniform1f(glGetUniformLocation(shader_programs[1], "spotLight.cutOff"), (float)cos(vmath::radians(12.5)));
+		//glUniform1f(glGetUniformLocation(shader_programs[1], "spotLight.outerCutOff"), (float)cos(vmath::radians(15.5)));
+
 		glUniform3fv(glGetUniformLocation(shader_programs[1], "spotLight.position"), 1, eye);
 		glUniform3fv(glGetUniformLocation(shader_programs[1], "spotLight.direction"), 1, center - eye);
 		glUniform3f(glGetUniformLocation(shader_programs[1], "spotLight.ambient"), 0.0f, 0.0f, 0.0f);
@@ -395,13 +421,23 @@ public:
 
 
 		// 피라미드 (광원) 그리기 ---------------------------------------
+		//float move_y = (float)cos(currentTime) * 0.2f + 0.5f;
+		//float scaleFactor = 0.05f;// (float)cos(currentTime)*0.05f + 0.2f;
+		//vmath::mat4 transform = vmath::translate(lightPosition) *
+		//	vmath::rotate(angle * 0.5f, 0.0f, 1.0f, 0.0f) *
+		//	vmath::scale(scaleFactor, scaleFactor, scaleFactor);
+
 		glUseProgram(shader_programs[2]);
 
 		glUniform3fv(glGetUniformLocation(shader_programs[2], "color"), 1, lightColor);
 		glUniformMatrix4fv(glGetUniformLocation(shader_programs[2], "projection"), 1, GL_FALSE, projM);
 		glUniformMatrix4fv(glGetUniformLocation(shader_programs[2], "view"), 1, GL_FALSE, lookAt);
 
+		// glUniformMatrix4fv(glGetUniformLocation(shader_programs[2], "model"), 1, GL_FALSE, transform);
+
 		glBindVertexArray(VAOs[2]);
+
+		// glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
 		for (int i = 0; i < 2; i++) {
 			vmath::mat4 transform = vmath::translate(pointLightPositions[i]) * vmath::scale(0.05f);
 			glUniformMatrix4fv(glGetUniformLocation(shader_programs[2], "model"), 1, GL_FALSE, transform);
--- CGP11-practice1/_myApp_/basic_lighting_fs.glsl
+++ CGP11-practice1/_myApp_/basic_lighting_fs.glsl
@@ -1,5 +1,135 @@
 #version 430 core
 
+//in vec3 vsNormal;
+//in vec3 vsPos;
+//in vec2 vsTexCoord;
+//
+//uniform vec3 viewPos;
+//
+//struct Material {
+//    sampler2D diffuse;  // Diffuse Map
+//    sampler2D specular; // Specular Map
+//    float shininess;
+//};
+//uniform Material material;
+//
+////1 =============================
+//struct DirLight {
+//    vec3 direction;
+//
+//    vec3 ambient;
+//    vec3 diffuse;
+//    vec3 specular;
+//};
+//uniform DirLight dirLight;
+//// ===============================
+//
+////2 ==============================
+//struct PointLight {
+//    vec3 position;
+//    float c1, c2;
+//
+//    vec3 ambient;
+//    vec3 diffuse;
+//    vec3 specular;
+//};
+//#define NUM_POINT_LIGHTS 2
+//uniform PointLight pointLights[NUM_POINT_LIGHTS];
+//// ====================================
+//
+////3 ================================
+//struct SpotLight {
+//    vec3 position;
+//    vec3 direction;
+//    float cutOff, outerCutOff;
+//    float c1, c2;
+//
+//    vec3 ambient;
+//    vec3 diffuse;
+//    vec3 specular;
+//};
+//uniform SpotLight spotLight;
+//// ==================================
+//
+//out vec4 fragColor;
+
+//void main()
+//{
+//    // --- Phong Lighting Model 1 ---
+//    // 1. Ambient
+//    vec3 ambient = dirLight.ambient * vec3(texture(material.diffuse, vsTexCoord));
+//
+//    // 2. Diffuse
+//    vec3 norm = normalize(vsNormal);  // 단위벡터로 만들기 위해 정규화
+//    vec3 lightDir = normalize(-dirLight.direction);  // 광원을 향하는 벡터 구하고 정규화
+//    float diff = max(dot(norm, lightDir), 0.0);  // 내적을 통해 밝기 계산 음수값이 나올 경우, 0으로 만들기 위해 max()
+//    vec3 diffuse = dirLight.diffuse * diff * vec3(texture(material.diffuse, vsTexCoord));
+//
+//    // 3. Specular
+//    vec3 viewDir = normalize(viewPos - vsPos); // 카메라를 향하는 벡터
+//    vec3 reflectDir = reflect(-lightDir, norm); // 표면에 반사된 광원 벡터
+//    // 표면에 반사된 광원 벡터를 구하는 방법: reflect(광원을 향하는 벡터, 노멀벡터)
+//    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+//    vec3 specular = dirLight.specular * spec * vec3(texture(material.specular, vsTexCoord));
+//
+//    vec3 result = ambient + diffuse + specular;
+//    // 최종 출력 코드
+//    fragColor = vec4(result, 1.0);
+
+
+//    // --- Phong Lighting Model 2 ---
+//    float dist = length(pointLights[0].position - vsPos);
+//    float attenuation = 1.0 / (1.0+pointLights[0].c1*dist + pointLights[0].c2*dist*dist);
+//
+//    // 1. Ambient
+//    vec3 ambient = pointLights[0].ambient * vec3(texture(material.diffuse, vsTexCoord));
+//
+//    // 2. Diffuse
+//    vec3 norm = normalize(vsNormal); 
+//    vec3 lightDir = normalize(pointLights[0].position - vsPos);  
+//    float diff = max(dot(norm, lightDir), 0.0); 
+//    vec3 diffuse = pointLights[0].diffuse * diff * vec3(texture(material.diffuse, vsTexCoord));
+//
+//    // 3. Specular
+//    vec3 viewDir = normalize(viewPos - vsPos); 
+//    vec3 reflectDir = reflect(-lightDir, norm); /
+//    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+//    vec3 specular = pointLights[0].specular * spec * vec3(texture(material.specular, vsTexCoord));
+//
+//    vec3 result = (ambient + diffuse + specular) * attenuation;
+//    fragColor = vec4(result, 1.0);
+
+
+//    // --- Phong Lighting Model 3 ---
+//    vec3 lightDir = normalize(spotLight.position - vsPos);  
+//    float theta = dot(lightDir, normalize(-spotLight.direction));
+//    float epsilon = spotLight.cutOff - spotLight.outerCutOff;
+//    float intensity = clamp((theta - spotLight.outerCutOff) / epsilon, 0.0, 1.0);
+//    float dist = length(spotLight.position - vsPos);
+//    float attenuation = 1.0 / (1.0+spotLight.c1*dist + spotLight.c2*dist*dist);
+//
+//    // 1. Ambient
+//    vec3 ambient = spotLight.ambient * vec3(texture(material.diffuse, vsTexCoord));
+//
+//    // 2. Diffuse
+//    vec3 norm = normalize(vsNormal);  
+//    float diff = max(dot(norm, lightDir), 0.0); 
+//    vec3 diffuse = spotLight.diffuse * diff * vec3(texture(material.diffuse, vsTexCoord));
+//    diffuse *= intensity; // ambient는 건들지 않음, 최소한의 빛을 유지하기 위해
+//
+//    // 3. Specular
+//    vec3 viewDir = normalize(viewPos - vsPos);
+//    vec3 reflectDir = reflect(-lightDir, norm); 
+//    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+//    vec3 specular = spotLight.specular * spec * vec3(texture(material.specular, vsTexCoord));
+//    specular *= intensity; // ambient는 건들지 않음, 최소한의 빛을 유지하기 위해
+//
+//    vec3 result = ( ambient + diffuse + specular ) * attenuation;
+//    fragColor = vec4(result, 1.0);
+//}
+
+
+
 in vec3 vsNormal;  
 in vec3 vsPos;  
 in vec2 vsTexCoord;
